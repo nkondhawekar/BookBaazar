9700ae0fb5632a6cafcba37f2ac522b67ee33edb
pranavthamil <thamil.p@northeastern.edu>
2025-04-15 22:58:12 -0400

    trendingbooks

---
+++ app/src/pages/31_Bookstore_TrendingBooks.py
@@ -0,0 +1,16 @@
+# 31_Bookstore_TrendingBooks.py
+# test
+
+import streamlit as st
+import requests
+
+st.set_page_config(page_title="Trending Books Dashboard", layout="wide")
+st.title("Trending Books Dashboard")
+response = requests.get("http://localhost:5000/bookstore/trending-books")
+if response.status_code == 200:
+    trending = response.json()
+    st.dataframe(trending)
+else:
+    st.error("Error fetching trending books.")
+
+st.markdown("[Back to Bookstore Home](?page=30_Bookstore_Home)")
